4e8f2ac28d438ca5015649c8834152d09744d110
Genki Hashimoto <knct.gen@gmail.com>
2015-11-11 15:52:45 +0900

    諦めました

--- keylogger_test.py
+++ keylogger_test.py
@@ -1,6 +1,12 @@
 import pygame
+from pygame.locals import *
 import sys
 
 pygame.init()
 
-print "^^"
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT: sys.exit
+		if event.type == KEYDOWN:
+			if event.key == K_a:
+				print "a"
